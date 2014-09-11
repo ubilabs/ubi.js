cf1c675af37d0ad1888d67e6a74b9e8b6c150153
Frank Mecklenburg Ubilabs <mecklenburg@ubilabs.net>
2014-09-11 16:43:29 +0200

    Update lintspaces spec.

--- tasks/lintspaces.js
+++ tasks/lintspaces.js
@@ -1,12 +1,19 @@
 module.exports = {
   options: {
+    newline: true,
+    // TODO: Issue with ignore comments.
+    // https://github.com/schorfES/grunt-lintspaces/issues/20
+    // newlineMaximum: 1,
+    ignores: [
+      'js-comments'
+    ],
     editorconfig: '.editorconfig'
   },
   gruntfile: {
     src: ['Gruntfile.js', 'tasks/{,*/}*.js']
   },
   lib: {
-    src: ['lib/**/*.js']
+    src: ['lib/**/*.js'],
   },
   test: {
     src: ['test/spec/**/*.js']
