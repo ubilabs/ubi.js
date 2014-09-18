7be27eee4db358bc96dd71b0b8f776348401704c
Frank Mecklenburg Ubilabs <mecklenburg@ubilabs.net>
2014-09-18 15:27:51 +0200

    feat(build): add build to git

--- tasks/gitcommit.js
+++ tasks/gitcommit.js
@@ -1,13 +1,23 @@
 module.exports = {
   options: {
-    message: 'chore(docs): Update docs for release',
     ignoreEmpty: true,
     noVerify: true,
     noStatus: true
   },
   docs: {
+    options: {
+      message: 'Update docs for release',
+    },
     files: {
       src: ['docs/*']
+    }
+  },
+  build: {
+    options: {
+      message: 'Update build for release',
+    },
+    files: {
+      src: ['dist/*']
     }
   }
 };
